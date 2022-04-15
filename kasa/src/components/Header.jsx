2e573f4d8d1f5fe7287d31f888d6cd0184498aba
Pierre Petillot <petill_p@etna-alternance.net>
2022-04-15 18:00:19 +0200

    soulignement

--- kasa/src/components/Header.jsx
+++ kasa/src/components/Header.jsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/images/logo.jpg";
 import "../style/Header.css";
 
 function Header() {
 	return (
 		<header className="header-wrapper">
-			<img src={logo} className="logo" alt="logo" />
+			<Link to="/">
+				<img src={logo} className="logo" alt="logo" />
+			</Link>
 			<div className="header-nav">
-				<Link to="/">
+				<NavLink
+					to="/"
+					className={({ isActive }) => (isActive ? "active" : "inactive")}
+				>
 					<h3 className="nav-item">Accueil</h3>
-				</Link>
-				<Link to="about">
+				</NavLink>
+				<NavLink
+					to="about"
+					className={({ isActive }) => (isActive ? "active" : "inactive")}
+				>
 					<h3 className="nav-item">A propos</h3>
-				</Link>
+				</NavLink>
 			</div>
 		</header>
 	);
